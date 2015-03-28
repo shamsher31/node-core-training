dcacea88d0e231e2254b807d796db12656ff36ba
Shamsher <ansarisam47@gmail.com>
2015-03-28 17:18:37 +0530

    Send response with custome header information

---
+++ res-with-custom-header.js
@@ -0,0 +1,22 @@
+const http = require('http');
+
+var server = http.createServer(resWithHeader);
+server.listen(8000);
+
+console.log('Server running and waiting for request');
+
+function resWithHeader(req, res) {
+	
+	console.log('I got the request');
+
+	var body = 'NodeJs response with some custom header information';
+	var contentLength = body.length;
+
+	res.writeHead(200, {
+		'Content-Type' : 'text/plain',
+		'Content-Length' : contentLength
+	});
+
+	res.end(body);
+
+}

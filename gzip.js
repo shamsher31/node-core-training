49a7a99fd81f018a277015424d52dddb76bb38f5
Shamsher <ansarisam47@gmail.com>
2015-12-09 23:13:39 +0530

    Refactor gzip example to add promises

--- gzip.js
+++ gzip.js
@@ -1,13 +1,24 @@
-var fs = require('fs'),
+var Promise = require('bluebird'),
+		fs = Promise.promisifyAll(require('fs')),
 		zlib = require('zlib'),
 		filename;
 	
 filename = process.argv[2];
 
-fs.readFile(filename, function(err, buffer) {
-	zlib.gzip(buffer, function(err, buffer) {
-		fs.writeFile(filename + '.gz', buffer, function(err) {
-			console.log('File successfully compressed');
-		});
-	});
-});
+fs.readFileAsync(filename)
+	.then(function(buffer) {
+		return new Promise(function(resolve, reject) {
+			return zlib.gzip(buffer, function(err, buffer) {
+				resolve(buffer);
+			});	
+		});	
+	})
+	.then(function(buffer) {
+		return fs.writeFileAsync(filename + '.gz', buffer);
+	})
+	.then(function() {
+		console.log('File successfully compressed');
+	})
+	.catch(function(err) {
+		console.log(err);
+	})

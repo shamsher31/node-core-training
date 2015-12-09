09383cbcfb025642e41ee4a6af623468903ed664
Shamsher <ansarisam47@gmail.com>
2015-12-09 23:25:16 +0530

    Refactor gzip fucntion

--- gzip.js
+++ gzip.js
@@ -7,11 +7,7 @@ filename = process.argv[2];
 
 fs.readFileAsync(filename)
 	.then(function(buffer) {
-		return new Promise(function(resolve, reject) {
-			return zlib.gzip(buffer, function(err, buffer) {
-				resolve(buffer);
-			});	
-		});	
+		return gzip(buffer);
 	})
 	.then(function(buffer) {
 		return fs.writeFileAsync(filename + '.gz', buffer);
@@ -21,4 +17,12 @@ fs.readFileAsync(filename)
 	})
 	.catch(function(err) {
 		console.log(err);
-	})
+	});
+	
+function gzip(buffer) {
+	return new Promise(function(resolve, reject) {
+		return zlib.gzip(buffer, function(err, buffer) {
+			resolve(buffer);
+		});	
+	});
+}

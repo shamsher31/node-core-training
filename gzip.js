5a1bf1ddb0d72337d2d591aad9f2602bb91ce3e8
Shamsher <ansarisam47@gmail.com>
2015-12-10 10:03:41 +0530

    Refcator gzip to use power of stream

--- gzip.js
+++ gzip.js
@@ -1,28 +1,13 @@
-var Promise = require('bluebird'),
-		fs = Promise.promisifyAll(require('fs')),
+var fs = require('fs'),
 		zlib = require('zlib'),
-		filename;
+		gzip = zlib.createGzip(),
+		filename,
+		input,
+		output;
 	
 filename = process.argv[2];
 
-fs.readFileAsync(filename)
-	.then(function(buffer) {
-		return gzip(buffer);
-	})
-	.then(function(buffer) {
-		return fs.writeFileAsync(filename + '.gz', buffer);
-	})
-	.then(function() {
-		console.log('File successfully compressed');
-	})
-	.catch(function(err) {
-		console.log(err);
-	});
-	
-function gzip(buffer) {
-	return new Promise(function(resolve, reject) {
-		return zlib.gzip(buffer, function(err, buffer) {
-			resolve(buffer);
-		});	
-	});
-}
+input = fs.createReadStream(filename);
+output = fs.createWriteStream(filename + '.gz');
+
+input.pipe(gzip).pipe(output);
